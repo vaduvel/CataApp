a9bccd114c977f021e88d017d5d4d0327b204666
vaduvel <vaduvadaniel10@yahoo.com>
2026-08-18 21:40:54 +0300

    build(m7): pregateste backup, poze si workmanager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	alias(libs.plugins.android.application)
 	alias(libs.plugins.kotlin.android)
 	alias(libs.plugins.kotlin.compose)
+	alias(libs.plugins.kotlin.serialization)
 	alias(libs.plugins.ksp)
 }
 
@@ -70,6 +71,10 @@ dependencies {
 	implementation(libs.androidx.room.runtime)
 	implementation(libs.androidx.room.ktx)
 	ksp(libs.androidx.room.compiler)
+
+	implementation(libs.androidx.work.runtime.ktx)
+	implementation(libs.kotlinx.serialization.json)
+	implementation(libs.coil.compose)
 
 	debugImplementation(libs.androidx.ui.tooling)
 	debugImplementation(libs.androidx.ui.test.manifest)
