1148f7e3bc0dd0355bc130bbbe6ced4230283372
Vaduva George <65630341+vaduvel@users.noreply.github.com>
2026-08-20 11:40:34 +0300

    chore(release): pregateste versiunea 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "com.emanus.lucrari"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 10
-		versionName = "0.10.0-m8"
+		versionCode = 11
+		versionName = "1.0.0"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 
